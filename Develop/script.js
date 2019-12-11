bb4b7a24fda29faef490708593bb49ba56747e1a
Isabel Uriarte <uriarte.isabel@gmail.com>
2019-12-10 18:06:15 -0800

    formatting

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,7 @@ $(".description").each(function () {
 
     } else if (plannerHour === currentHour) {
         $(this).addClass("present");
+        
     } else {
         $(this).addClass("future");
     }
