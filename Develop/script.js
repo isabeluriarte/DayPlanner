069d807287fcf7825b86382d595d37d891a7be86
Isabel Uriarte <uriarte.isabel@gmail.com>
2019-12-10 18:05:03 -0800

    Updated js to change colors with moment.js and save tasks to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,27 @@
 var currentDay = $('#currentDay')
 currentDay.text(moment().format("dddd, MMMM Do"))
 
-// think about 24 hour clock (i.e. 1pm = 13)
+var currentHour = parseInt(moment().format('H'));
+
+$(".description").each(function () {
+    var plannerHour = parseInt($(this).attr("data-hour"));
+
+    var task = localStorage.getItem(plannerHour)
+    $(this).val(task)
+
+    if (plannerHour < currentHour) {
+        $(this).addClass("past");
+
+    } else if (plannerHour === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+})
+
+$(".saveBtn").click(function () {
+    var box = $(this).siblings(".description")
+    var task = box.val()
+
+    localStorage.setItem(box.attr("data-hour"), task)
+})
